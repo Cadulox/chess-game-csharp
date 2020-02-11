a106b1c5ba543e7d6ab21980286e45d01a90a944
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-02-10 23:29:43 -0300

    Starting to implement the board

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -5,11 +5,7 @@ namespace chess_console {
     class Program {
         static void Main(string[] args) {
 
-            Position P;
-
-            P = new Position(3, 4);
-
-            Console.WriteLine("Position: " + P);
+            Board board = new Board(8, 8);
 
             Console.ReadLine();
         }
