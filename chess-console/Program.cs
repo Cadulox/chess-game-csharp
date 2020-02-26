4dc240d877a54f6733a15bde5d9f39c42e52df40
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-02-25 23:02:37 -0300

    Improving the implementation of the printBoard method

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -6,12 +6,20 @@ namespace chess_console {
     class Program {
         static void Main(string[] args) {
 
-            ChessPosition pos = new ChessPosition('c', 7);
+            try {
+                Board board = new Board(8, 8);
 
-            Console.WriteLine(pos);
+                board.putPiece(new Rook(board, Color.Black), new Position(0, 0));
+                board.putPiece(new Rook(board, Color.Black), new Position(1, 3));
+                board.putPiece(new King(board, Color.Black), new Position(0, 2));
 
-            Console.WriteLine(pos.toPosition());
+                board.putPiece(new Rook(board, Color.White), new Position(3, 5));
 
+                Screen.printBoard(board);
+            }
+            catch (BoardException e) {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
